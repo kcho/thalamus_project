bbd7d794df6f06e6e6572e52ad5da9ef931000e0
kcho <sky8671@gmail.com>
2017-03-08 19:59:03 +0900

    variables are collected at the top for easy future changes

--- script.sh
+++ script.sh
@@ -1,29 +1,38 @@
 #!/bin/bash
 for subj in $@
 do
+    # edit here for different folder structure
+    fsDir=${i}/FREESURFER
+    regDir=${i}/registration
+    dtiDir=${i}/DTI
+    dkiDir=${i}/DKI
+    roiDir=${i}/ROI
+    segDir=${i}/segmentation
+    bedpostDir=${i}/DTI.bedpostX
+
     # convert the Freesurfer output : brain.mgz --> brain.nii.gz 
-    if [ ! -e ${subj}/FREESURFER/mri/brain.nii.gz ]
+    if [ ! -e ${fsDir}/mri/brain.nii.gz ]
     then
         mri_convert --out_orientation RAS \
-            ${subj}/FREESURFER/mri/brain.mgz \
-            ${subj}/FREESURFER/mri/brain.nii.gz
+            ${fsDir}/mri/brain.mgz \
+            ${fsDir}/mri/brain.nii.gz
     fi
 
 
     # Registration
-    if [ ! -d ${subj}/registration ]
+    if [ ! -d ${regDir} ]
     then
-        mkdir ${subj}/registration
+        mkdir ${regDir}
     fi
 
     # FREESURFER output T1 --> diffusion tensor space
-    if [ ! -e ${subj}/registration/FREESURFERT1toNodif.mat ]
+    if [ ! -e ${regDir}/FREESURFERT1toNodif.mat ]
     then
         flirt \
-            -in ${subj}/FREESURFER/mri/brain.nii.gz \
-            -ref ${subj}/DTI/b0_brain.nii.gz \
-            -out ${subj}/registration/FREESURFERT1toNodif \
-            -omat ${subj}/registration/FREESURFERT1toNodif.mat \
+            -in ${fsDir}/mri/brain.nii.gz \
+            -ref ${dtiDir}/b0_brain.nii.gz \
+            -out ${regDir}/FREESURFERT1toNodif \
+            -omat ${regDir}/FREESURFERT1toNodif.mat \
             -bins 256 \
             -cost mutualinfo \
             -searchrx -180 180 \
@@ -33,13 +42,13 @@ do
     fi
 
     # FREESURFER output T1 --> diffusion kurtosis space
-    if [ ! -e ${subj}/registration/FREESURFERT1toDKINodif.mat ]
+    if [ ! -e ${regDir}/FREESURFERT1toDKINodif.mat ]
     then
         flirt \
-            -in ${subj}/FREESURFER/mri/brain.nii.gz \
-            -ref ${subj}/DKI/b0_brain.nii.gz \
-            -out ${subj}/registration/FREESURFERT1toDKINodif \
-            -omat ${subj}/registration/FREESURFERT1toDKINodif.mat \
+            -in ${fsDir}/mri/brain.nii.gz \
+            -ref ${dkiDir}/b0_brain.nii.gz \
+            -out ${regDir}/FREESURFERT1toDKINodif \
+            -omat ${regDir}/FREESURFERT1toDKINodif.mat \
             -bins 256 \
             -cost mutualinfo \
             -searchrx -180 180 \
@@ -50,12 +59,12 @@ do
 
 
     # ROI extraction
-    if [ ! -d ${subj}/ROI ] 
+    if [ ! -d ${roiDir} ] 
     then
-        mkdir ${subj}/ROI
+        mkdir ${roiDir}
     fi
 
-    if [ ! -e ${subj}/ROI/rh_OCC.nii.gz ]
+    if [ ! -e ${roiDir}/rh_OCC.nii.gz ]
     then
         python roiExtraction.py -S ${subj}
     else
@@ -64,48 +73,80 @@ do
 
 
     #Segmentation
-    if [ ! -d ${subj}/segmentation ]
+    if [ ! -d ${segDir} ]
     then
-        mkdir ${subj}/segmentation
+        mkdir ${segDir}
     else
-        if [ ! -d ${subj}/segmentation/left ]
+        if [ ! -d ${segDir}/left ]
         then
-            mkdir ${subj}/segmentation/left
-            mkdir ${subj}/segmentation/right
+            mkdir ${segDir}/left
+            mkdir ${segDir}/right
         fi
     fi
 
-    if [ ! -e ${subj}/segmentation/left/fdt_paths.nii.gz ]
+    if [ ! -e ${segDir}/left/fdt_paths.nii.gz ]
     then
-        rm -rf ${subj}/segmentation/left 
-        mkdir -p ${subj}/segmentation/left
-        echo ${subj}/ROI/lh_LPFC.nii.gz > ${subj}/segmentation/left/targets.txt
-        echo ${subj}/ROI/lh_LTC.nii.gz >> ${subj}/segmentation/left/targets.txt
-        echo ${subj}/ROI/lh_MPFC.nii.gz >> ${subj}/segmentation/left/targets.txt
-        echo ${subj}/ROI/lh_MTC.nii.gz >> ${subj}/segmentation/left/targets.txt
-        echo ${subj}/ROI/lh_OCC.nii.gz >> ${subj}/segmentation/left/targets.txt
-        echo ${subj}/ROI/lh_OFC.nii.gz >> ${subj}/segmentation/left/targets.txt
-        echo ${subj}/ROI/lh_PC.nii.gz >> ${subj}/segmentation/left/targets.txt
-        echo ${subj}/ROI/lh_SMC.nii.gz >> ${subj}/segmentation/left/targets.txt
-        probtrackx --mode=seedmask -x ${subj}/ROI/lh_thalamus.nii.gz -l -c 0.2 -s 2000 --steplength=0.5 -P 5000 --xfm=${subj}/registration/FREESURFERT1toNodif.mat --forcedir --opd -s ${subj}/DTI.bedpostX/merged -m ${subj}/DTI.bedpostX/nodif_brain_mask --dir=${subj}/segmentation/left --targetmasks=${subj}/segmentation/left/targets.txt --os2t
+        rm -rf ${segDir}/left 
+        mkdir -p ${segDir}/left
+        echo ${roiDir}/lh_LPFC.nii.gz > ${segDir}/left/targets.txt
+        echo ${roiDir}/lh_LTC.nii.gz >> ${segDir}/left/targets.txt
+        echo ${roiDir}/lh_MPFC.nii.gz >> ${segDir}/left/targets.txt
+        echo ${roiDir}/lh_MTC.nii.gz >> ${segDir}/left/targets.txt
+        echo ${roiDir}/lh_OCC.nii.gz >> ${segDir}/left/targets.txt
+        echo ${roiDir}/lh_OFC.nii.gz >> ${segDir}/left/targets.txt
+        echo ${roiDir}/lh_PC.nii.gz >> ${segDir}/left/targets.txt
+        echo ${roiDir}/lh_SMC.nii.gz >> ${segDir}/left/targets.txt
+
+        probtrackx \
+            --mode=seedmask \
+            -x ${roiDir}/lh_thalamus.nii.gz \
+            -l \
+            -c 0.2 \
+            -s 2000 \
+            --steplength=0.5 \
+            -P 5000 \
+            --xfm=${regDir}/FREESURFERT1toNodif.mat \
+            --forcedir \
+            --opd \
+            -s ${bedpostDir}/merged \
+            -m ${bedpostDir}/nodif_brain_mask \
+            --dir=${segDir}/left \
+            --targetmasks=${segDir}/left/targets.txt \
+            --os2t
     else
         echo ${subj} segmentation on the left done
     fi
 
 
-    if [ ! -e ${subj}/segmentation/right/fdt_paths.nii.gz ]
+    if [ ! -e ${segDir}/right/fdt_paths.nii.gz ]
     then
         rm -rf ${subj}/segmentation/right
         mkdir -p ${subj}/segmentation/right
-        echo ${subj}/ROI/rh_LPFC.nii.gz > ${subj}/segmentation/right/targets.txt
-        echo ${subj}/ROI/rh_LTC.nii.gz >> ${subj}/segmentation/right/targets.txt
-        echo ${subj}/ROI/rh_MPFC.nii.gz >> ${subj}/segmentation/right/targets.txt
-        echo ${subj}/ROI/rh_MTC.nii.gz >> ${subj}/segmentation/right/targets.txt
-        echo ${subj}/ROI/rh_OCC.nii.gz >> ${subj}/segmentation/right/targets.txt
-        echo ${subj}/ROI/rh_OFC.nii.gz >> ${subj}/segmentation/right/targets.txt
-        echo ${subj}/ROI/rh_PC.nii.gz >> ${subj}/segmentation/right/targets.txt
-        echo ${subj}/ROI/rh_SMC.nii.gz >> ${subj}/segmentation/right/targets.txt
-        probtrackx --mode=seedmask -x ${subj}/ROI/rh_thalamus.nii.gz -l -c 0.2 -s 2000 --steplength=0.5 -P 5000 --xfm=${subj}/registration/FREESURFERT1toNodif.mat --forcedir --opd -s ${subj}/DTI.bedpostX/merged -m ${subj}/DTI.bedpostX/nodif_brain_mask --dir=${subj}/segmentation/right --targetmasks=${subj}/segmentation/right/targets.txt --os2t 
+        echo ${roiDir}/rh_LPFC.nii.gz > ${segDir}/right/targets.txt
+        echo ${roiDir}/rh_LTC.nii.gz >> ${segDir}/right/targets.txt
+        echo ${roiDir}/rh_MPFC.nii.gz >> ${segDir}/right/targets.txt
+        echo ${roiDir}/rh_MTC.nii.gz >> ${segDir}/right/targets.txt
+        echo ${roiDir}/rh_OCC.nii.gz >> ${segDir}/right/targets.txt
+        echo ${roiDir}/rh_OFC.nii.gz >> ${segDir}/right/targets.txt
+        echo ${roiDir}/rh_PC.nii.gz >> ${segDir}/right/targets.txt
+        echo ${roiDir}/rh_SMC.nii.gz >> ${segDir}/right/targets.txt
+
+        probtrackx \
+            --mode=seedmask \
+            -x ${roiDir}/rh_thalamus.nii.gz \
+            -l \
+            -c 0.2 \
+            -s 2000 \
+            --steplength=0.5 \
+            -P 5000 \
+            --xfm=${regDir}/FREESURFERT1toNodif.mat \
+            --forcedir \
+            --opd \
+            -s ${bedpostDir}/merged \
+            -m ${bedpostDir}/nodif_brain_mask \
+            --dir=${segDir}/right \
+            --targetmasks=${segDir}/right/targets.txt \
+            --os2t 
     else
         echo ${subj} segmentation on the right done
     fi
@@ -115,35 +156,35 @@ do
     for side in left right
     do
         echo ${side}
-        if [ ! -e ${subj}/segmentation/${side}/biggest.nii.gz ]
+        if [ ! -e ${segDir}/${side}/biggest.nii.gz ]
         then
             echo 'find the biggest in ${subj} ${side}'
-            find_the_biggest ${subj}/segmentation/${side}/seed* \
-                ${subj}/segmentation/${side}/biggest.nii.gz
+            find_the_biggest ${segDir}/${side}/seed* \
+                ${segDir}/${side}/biggest.nii.gz
         fi
 
         for thr in 5 10 20 90 95
         do
-            if [ ! -d ${subj}/segmentation/${side}/${thr}thrP ]
+            if [ ! -d ${segDir}/${side}/${thr}thrP ]
             then
-                mkdir ${subj}/segmentation/${side}/${thr}thrP
+                mkdir ${segDir}/${side}/${thr}thrP
 
             fi
 
-            if [ ! -e ${subj}/segmentation/${side}/${thr}thrP/*SMC* ]
+            if [ ! -e ${segDir}/${side}/${thr}thrP/*SMC* ]
             then
-                for imgs in ${subj}/segmentation/${side}/seeds*
+                for imgs in ${segDir}/${side}/seeds*
                 do
                     filename=`basename "${imgs}"`
                     echo ${subj} ${side} ${filename} ${thr}% thresholding
                     echo ${filename}
 
                     fslmaths ${imgs} -thrP ${thr} \
-                        ${subj}/segmentation/${side}/${thr}thrP/${thr}_${filename}
+                        ${segDir}/${side}/${thr}thrP/${thr}_${filename}
                 done
             
-                find_the_biggest ${subj}/segmentation/${side}/${thr}thrP/*seeds* \
-                    ${subj}/segmentation/${side}/${thr}thrP/${thr}_biggest.nii.gz
+                find_the_biggest ${segDir}/${side}/${thr}thrP/*seeds* \
+                    ${segDir}/${side}/${thr}thrP/${thr}_biggest.nii.gz
             else
                 echo ${subj} segmentation thresholding done
             fi
