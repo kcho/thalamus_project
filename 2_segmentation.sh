6a47146bb459cad03d80a7275f220520c2dc5ea6
kcho <sky8671@gmail.com>
2017-03-08 21:19:49 +0900

    optimised for parallel processing

--- 2_segmentation.sh
+++ 2_segmentation.sh
@@ -1,193 +1,108 @@
-#!/bin/bash
-for subj in $@
+#!/bin/sh
+Usage() {
+    echo ""
+    echo "Usage: 2_segmentation <subjDir> <lh>"
+    echo ""
+    exit 1
+}
+
+[ "$1" = "" ] && Usage
+
+# edit here for different folder structure
+subj=${1}
+fsDir=${subj}/FREESURFER
+regDir=${subj}/registration
+dtiDir=${subj}/DTI
+dkiDir=${subj}/DKI
+roiDir=${subj}/ROI
+segDir=${subj}/segmentation
+bedpostDir=${subj}/DTI.bedpostX
+
+if [ $2 == 'lh' ]
+then
+    side=left
+elif [ $2 == 'rh' ]
+then
+    side=right
+else
+    Usage
+fi
+
+#Segmentation
+if [ ! -d ${segDir} ]
+then
+    mkdir ${segDir}
+else
+    if [ ! -d ${segDir}/${side} ]
+    then
+        mkdir ${segDir}/${side}
+    fi
+fi
+
+if [ ! -e ${segDir}/${side}/fdt_paths.nii.gz ]
+then
+    rm -rf ${segDir}/${side} 
+    mkdir -p ${segDir}/${side}
+    echo ${roiDir}/lh_LPFC.nii.gz > ${segDir}/${side}/targets.txt
+    echo ${roiDir}/lh_LTC.nii.gz >> ${segDir}/${side}/targets.txt
+    echo ${roiDir}/lh_MPFC.nii.gz >> ${segDir}/${side}/targets.txt
+    echo ${roiDir}/lh_MTC.nii.gz >> ${segDir}/${side}/targets.txt
+    echo ${roiDir}/lh_OCC.nii.gz >> ${segDir}/${side}/targets.txt
+    echo ${roiDir}/lh_OFC.nii.gz >> ${segDir}/${side}/targets.txt
+    echo ${roiDir}/lh_PC.nii.gz >> ${segDir}/${side}/targets.txt
+    echo ${roiDir}/lh_SMC.nii.gz >> ${segDir}/${side}/targets.txt
+
+    probtrackx \
+        --mode=seedmask \
+        -x ${roiDir}/lh_thalamus.nii.gz \
+        -l \
+        -c 0.2 \
+        -s 2000 \
+        --steplength=0.5 \
+        -P 5000 \
+        --xfm=${regDir}/FREESURFERT1toNodif.mat \
+        --forcedir \
+        --opd \
+        -s ${bedpostDir}/merged \
+        -m ${bedpostDir}/nodif_brain_mask \
+        --dir=${segDir}/${side} \
+        --targetmasks=${segDir}/${side}/targets.txt \
+        --os2t
+else
+    echo ${subj} segmentation on the ${side} done
+fi
+
+# Output thresholding
+if [ ! -e ${segDir}/${side}/biggest.nii.gz ]
+then
+    echo 'find the biggest in ${subj} ${side}'
+    find_the_biggest ${segDir}/${side}/seed* \
+        ${segDir}/${side}/biggest.nii.gz
+fi
+
+for thr in 5 10 20 90 95
 do
-    # edit here for different folder structure
-    fsDir=${i}/FREESURFER
-    regDir=${i}/registration
-    dtiDir=${i}/DTI
-    dkiDir=${i}/DKI
-    roiDir=${i}/ROI
-    segDir=${i}/segmentation
-    bedpostDir=${i}/DTI.bedpostX
-
-    # convert the Freesurfer output : brain.mgz --> brain.nii.gz 
-    if [ ! -e ${fsDir}/mri/brain.nii.gz ]
+    if [ ! -d ${segDir}/${side}/${thr}thrP ]
     then
-        mri_convert --out_orientation RAS \
-            ${fsDir}/mri/brain.mgz \
-            ${fsDir}/mri/brain.nii.gz
+        mkdir ${segDir}/${side}/${thr}thrP
+
     fi
 
-
-    # Registration
-    if [ ! -d ${regDir} ]
+    if [ ! -e ${segDir}/${side}/${thr}thrP/*SMC* ]
     then
-        mkdir ${regDir}
-    fi
-
-    # FREESURFER output T1 --> diffusion tensor space
-    if [ ! -e ${regDir}/FREESURFERT1toNodif.mat ]
-    then
-        flirt \
-            -in ${fsDir}/mri/brain.nii.gz \
-            -ref ${dtiDir}/b0_brain.nii.gz \
-            -out ${regDir}/FREESURFERT1toNodif \
-            -omat ${regDir}/FREESURFERT1toNodif.mat \
-            -bins 256 \
-            -cost mutualinfo \
-            -searchrx -180 180 \
-            -searchry -180 180 \
-            -searchrz -180 180 \
-            -dof 6  -interp trilinear
-    fi
-
-    # FREESURFER output T1 --> diffusion kurtosis space
-    if [ ! -e ${regDir}/FREESURFERT1toDKINodif.mat ]
-    then
-        flirt \
-            -in ${fsDir}/mri/brain.nii.gz \
-            -ref ${dkiDir}/b0_brain.nii.gz \
-            -out ${regDir}/FREESURFERT1toDKINodif \
-            -omat ${regDir}/FREESURFERT1toDKINodif.mat \
-            -bins 256 \
-            -cost mutualinfo \
-            -searchrx -180 180 \
-            -searchry -180 180 \
-            -searchrz -180 180 \
-            -dof 6  -interp trilinear
-    fi
-
-
-    # ROI extraction
-    if [ ! -d ${roiDir} ] 
-    then
-        mkdir ${roiDir}
-    fi
-
-    if [ ! -e ${roiDir}/rh_OCC.nii.gz ]
-    then
-        python roiExtraction.py -S ${subj}
-    else
-        echo ${subj} 'ROI extraction done'
-    fi
-
-
-    #Segmentation
-    if [ ! -d ${segDir} ]
-    then
-        mkdir ${segDir}
-    else
-        if [ ! -d ${segDir}/left ]
-        then
-            mkdir ${segDir}/left
-            mkdir ${segDir}/right
-        fi
-    fi
-
-    if [ ! -e ${segDir}/left/fdt_paths.nii.gz ]
-    then
-        rm -rf ${segDir}/left 
-        mkdir -p ${segDir}/left
-        echo ${roiDir}/lh_LPFC.nii.gz > ${segDir}/left/targets.txt
-        echo ${roiDir}/lh_LTC.nii.gz >> ${segDir}/left/targets.txt
-        echo ${roiDir}/lh_MPFC.nii.gz >> ${segDir}/left/targets.txt
-        echo ${roiDir}/lh_MTC.nii.gz >> ${segDir}/left/targets.txt
-        echo ${roiDir}/lh_OCC.nii.gz >> ${segDir}/left/targets.txt
-        echo ${roiDir}/lh_OFC.nii.gz >> ${segDir}/left/targets.txt
-        echo ${roiDir}/lh_PC.nii.gz >> ${segDir}/left/targets.txt
-        echo ${roiDir}/lh_SMC.nii.gz >> ${segDir}/left/targets.txt
-
-        probtrackx \
-            --mode=seedmask \
-            -x ${roiDir}/lh_thalamus.nii.gz \
-            -l \
-            -c 0.2 \
-            -s 2000 \
-            --steplength=0.5 \
-            -P 5000 \
-            --xfm=${regDir}/FREESURFERT1toNodif.mat \
-            --forcedir \
-            --opd \
-            -s ${bedpostDir}/merged \
-            -m ${bedpostDir}/nodif_brain_mask \
-            --dir=${segDir}/left \
-            --targetmasks=${segDir}/left/targets.txt \
-            --os2t
-    else
-        echo ${subj} segmentation on the left done
-    fi
-
-
-    if [ ! -e ${segDir}/right/fdt_paths.nii.gz ]
-    then
-        rm -rf ${subj}/segmentation/right
-        mkdir -p ${subj}/segmentation/right
-        echo ${roiDir}/rh_LPFC.nii.gz > ${segDir}/right/targets.txt
-        echo ${roiDir}/rh_LTC.nii.gz >> ${segDir}/right/targets.txt
-        echo ${roiDir}/rh_MPFC.nii.gz >> ${segDir}/right/targets.txt
-        echo ${roiDir}/rh_MTC.nii.gz >> ${segDir}/right/targets.txt
-        echo ${roiDir}/rh_OCC.nii.gz >> ${segDir}/right/targets.txt
-        echo ${roiDir}/rh_OFC.nii.gz >> ${segDir}/right/targets.txt
-        echo ${roiDir}/rh_PC.nii.gz >> ${segDir}/right/targets.txt
-        echo ${roiDir}/rh_SMC.nii.gz >> ${segDir}/right/targets.txt
-
-        probtrackx \
-            --mode=seedmask \
-            -x ${roiDir}/rh_thalamus.nii.gz \
-            -l \
-            -c 0.2 \
-            -s 2000 \
-            --steplength=0.5 \
-            -P 5000 \
-            --xfm=${regDir}/FREESURFERT1toNodif.mat \
-            --forcedir \
-            --opd \
-            -s ${bedpostDir}/merged \
-            -m ${bedpostDir}/nodif_brain_mask \
-            --dir=${segDir}/right \
-            --targetmasks=${segDir}/right/targets.txt \
-            --os2t 
-    else
-        echo ${subj} segmentation on the right done
-    fi
-
-
-    # Output thresholding
-    for side in left right
-    do
-        echo ${side}
-        if [ ! -e ${segDir}/${side}/biggest.nii.gz ]
-        then
-            echo 'find the biggest in ${subj} ${side}'
-            find_the_biggest ${segDir}/${side}/seed* \
-                ${segDir}/${side}/biggest.nii.gz
-        fi
-
-        for thr in 5 10 20 90 95
+        for imgs in ${segDir}/${side}/seeds*
         do
-            if [ ! -d ${segDir}/${side}/${thr}thrP ]
-            then
-                mkdir ${segDir}/${side}/${thr}thrP
+            filename=`basename "${imgs}"`
+            echo ${subj} ${side} ${filename} ${thr}% thresholding
+            echo ${filename}
 
-            fi
-
-            if [ ! -e ${segDir}/${side}/${thr}thrP/*SMC* ]
-            then
-                for imgs in ${segDir}/${side}/seeds*
-                do
-                    filename=`basename "${imgs}"`
-                    echo ${subj} ${side} ${filename} ${thr}% thresholding
-                    echo ${filename}
-
-                    fslmaths ${imgs} -thrP ${thr} \
-                        ${segDir}/${side}/${thr}thrP/${thr}_${filename}
-                done
-            
-                find_the_biggest ${segDir}/${side}/${thr}thrP/*seeds* \
-                    ${segDir}/${side}/${thr}thrP/${thr}_biggest.nii.gz
-            else
-                echo ${subj} segmentation thresholding done
-            fi
+            fslmaths ${imgs} -thrP ${thr} \
+                ${segDir}/${side}/${thr}thrP/${thr}_${filename}
         done
-    done
+    
+        find_the_biggest ${segDir}/${side}/${thr}thrP/*seeds* \
+            ${segDir}/${side}/${thr}thrP/${thr}_biggest.nii.gz
+    else
+        echo ${subj} segmentation thresholding done
+    fi
 done
