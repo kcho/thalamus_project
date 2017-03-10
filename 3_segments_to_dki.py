ab87e5354b3d14650c58e48464722578db1d6270
kcho <sky8671@gmail.com>
2017-03-10 15:33:05 +0900

    minor edit

--- 3_segments_to_dki.py
+++ 3_segments_to_dki.py
@@ -8,7 +8,7 @@ from multiprocessing import Pool
 
 def segments_to_dki(args):
     segLoc = 'segmentation'
-    kmeanLoc = 'DTI/kmean.nii'
+    kmeanLoc = 'DKI/kmean.nii'
     regMatLoc = 'registration/FREESURFERT1toDKINodif.mat'
 
     raw_seed_imgs = []
@@ -52,60 +52,3 @@ if __name__ == '__main__':
                         default = 1)
     args = parser.parse_args()
     segments_to_dki(args)
-
-#FLIRT version 6.0
-
-#Usage: flirt [options] -in <inputvol> -ref <refvol> -out <outputvol>
-       #flirt [options] -in <inputvol> -ref <refvol> -omat <outputmatrix>
-       #flirt [options] -in <inputvol> -ref <refvol> -applyxfm -init <matrix> -out <outputvol>
-
-  #Available options are:
-        #-in  <inputvol>                    (no default)
-        #-ref <refvol>                      (no default)
-        #-init <matrix-filname>             (input 4x4 affine matrix)
-        #-omat <matrix-filename>            (output in 4x4 ascii format)
-        #-out, -o <outputvol>               (default is none)
-        #-datatype {char,short,int,float,double}                    (force output data type)
-        #-cost {mutualinfo,corratio,normcorr,normmi,leastsq,labeldiff,bbr}        (default is corratio)
-        #-searchcost {mutualinfo,corratio,normcorr,normmi,leastsq,labeldiff,bbr}  (default is corratio)
-        #-usesqform                         (initialise using appropriate sform or qform)
-        #-displayinit                       (display initial matrix)
-        #-anglerep {quaternion,euler}       (default is euler)
-        #-interp {trilinear,nearestneighbour,sinc,spline}  (final interpolation: def - trilinear)
-        #-sincwidth <full-width in voxels>  (default is 7)
-        #-sincwindow {rectangular,hanning,blackman}
-        #-bins <number of histogram bins>   (default is 256)
-        #-dof  <number of transform dofs>   (default is 12)
-        #-noresample                        (do not change input sampling)
-        #-forcescaling                      (force rescaling even for low-res images)
-        #-minsampling <vox_dim>             (set minimum voxel dimension for sampling (in mm))
-        #-applyxfm                          (applies transform (no optimisation) - requires -init)
-        #-applyisoxfm <scale>               (as applyxfm but forces isotropic resampling)
-        #-paddingsize <number of voxels>    (for applyxfm: interpolates outside image by size)
-        #-searchrx <min_angle> <max_angle>  (angles in degrees: default is -90 90)
-        #-searchry <min_angle> <max_angle>  (angles in degrees: default is -90 90)
-        #-searchrz <min_angle> <max_angle>  (angles in degrees: default is -90 90)
-        #-nosearch                          (sets all angular search ranges to 0 0)
-        #-coarsesearch <delta_angle>        (angle in degrees: default is 60)
-        #-finesearch <delta_angle>          (angle in degrees: default is 18)
-        #-schedule <schedule-file>          (replaces default schedule)
-        #-refweight <volume>                (use weights for reference volume)
-        #-inweight <volume>                 (use weights for input volume)
-        #-wmseg <volume>                    (white matter segmentation volume needed by BBR cost function)
-        #-wmcoords <text matrix>            (white matter boundary coordinates for BBR cost function)
-        #-wmnorms <text matrix>             (white matter boundary normals for BBR cost function)
-        #-fieldmap <volume>                 (fieldmap image in rads/s - must be already registered to the reference image)
-        #-fieldmapmask <volume>             (mask for fieldmap image)
-        #-pedir <index>                     (phase encode direction of EPI - 1/2/3=x/y/z & -1/-2/-3=-x/-y/-z)
-        #-echospacing <value>               (value of EPI echo spacing - units of seconds)
-        #-bbrtype <value>                   (type of bbr cost function: signed [default], global_abs, local_abs)
-        #-bbrslope <value>                  (value of bbr slope)
-        #-setbackground <value>             (use specified background value for points outside FOV)
-        #-noclamp                           (do not use intensity clamping)
-        #-noresampblur                      (do not use blurring on downsampling)
-        #-2D                                (use 2D rigid body mode - ignores dof)
-        #-verbose <num>                     (0 is least and default)
-        #-v                                 (same as -verbose 1)
-        #-i                                 (pauses at each stage: default is off)
-        #-version                           (prints version number)
-        #-help
