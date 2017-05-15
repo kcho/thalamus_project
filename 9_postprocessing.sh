c5582ae215011d243a4b2ceb7ac0a8fe070b6a1d
kcho <sky8671@gmail.com>
2017-05-15 14:47:23 +0900

    test

--- 9_postprocessing.sh
+++ 9_postprocessing.sh
@@ -17,35 +17,40 @@ dtiDir=${subj}/DTI
 dkiDir=${subj}/DKI
 roiDir=${subj}/ROI
 tractDir=${subj}/thalamus_tractography
+tractDirMNI=${subj}/thalamus_tractography_MNI
 segDir=${subj}/segmentation
 bedpostDir=${subj}/DTI.bedpostX
 
 for side in left right
 do
-    reconImg=${tractDir}/${side}/fdt_matrix2_recontructed.nii.gz
-    reconImgMNI=${tractDir}/${side}/fdt_matrix2_recontructed_MNI.nii.gz
-    reconImgMNI_ds=${tractDir}/${side}/fdt_matrix2_recontructed_MNI_ds.nii.gz
-    #Segmentation
+    reconImg=${tractDir}/${side}/fdt_matrix2_reconstructed.nii.gz
+    reconImgMNI=${tractDir}/${side}/fdt_matrix2_reconstructed_MNI.nii.gz
+    reconImgMNI_ds=${tractDir}/${side}/fdt_matrix2_reconstructed_MNI_ds.nii.gz
+
+    # probtracks postprocessing
     if [ ! -e ${reconImg} ]
     then 
-        python tracktography/postprocessing/probtrackx_postprocessing.py ${tractDir}/${side}
+        python tracktography/postprocessing/probtrackx_postprocessing.py \
+            ${tractDirMNI}/${side} \
+            ${dtiDir}/b0.nii.gz
     fi
 
-    if [ ! -e ${reconImgMNI} ]
-    then
-        flirt \
-            -in ${reconImg} \
-            -ref ${FSLDIR}/data/standard/MNI152_T1_2mm.nii.gz \
-            -applyxfm -init ${regDir}/nodifToMNI.mat \
-            -out ${reconImgMNI}
-    fi
+    # do it in melodic
+    #if [ ! -e ${reconImgMNI} ]
+    #then
+        #flirt \
+            #-in ${reconImg} \
+            #-ref ${FSLDIR}/data/standard/MNI152_T1_2mm.nii.gz \
+            #-applyxfm -init ${regDir}/nodifToMNI.mat \
+            #-out ${reconImgMNI}
+    #fi
 
-    if [ ! -e ${reconImgMNI_ds} ]
-    then
-        flirt \
-            -in ${reconImgMNI} \
-            -ref ${reconImgMNI}
-            -applyisoxfm 3 \
-            -out ${reconImgMNI_ds}
-    fi
+    #if [ ! -e ${reconImgMNI_ds} ]
+    #then
+        #flirt \
+            #-in ${reconImgMNI} \
+            #-ref ${reconImgMNI} \
+            #-applyisoxfm 3 \
+            #-out ${reconImgMNI_ds}
+    #fi
 done
