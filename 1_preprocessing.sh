bad9957fb096fc5154168a8ea1df5c9d370305cb
kcho <sky8671@gmail.com>
2017-05-24 10:00:41 +0900

    fnirt pipeline added

--- 1_preprocessing.sh
+++ 1_preprocessing.sh
@@ -10,6 +10,10 @@ do
     segDir=${subj}/segmentation
     bedpostDir=${subj}/DTI.bedpostX
 
+    mni2mm=${FSLDIR}/data/standard/MNI152_T1_2mm_brain.nii.gz
+    mni2mmMask=${FSLDIR}/data/standard/MNI152_T1_2mm_brain_mask.nii.gz
+
+
     if [ ! -d ${bedpostDir} ]
     then
         bedpostx ${dtiDir}
@@ -30,13 +34,14 @@ do
     fi
 
     # FREESURFER output T1 --> diffusion tensor space
-    if [ ! -e ${regDir}/FREESURFERT1toNodif.mat ]
+    fs2nodifMat=${regDir}/FREESURFERT1toNodif.mat
+    if [ ! -e ${fs2nodifMat} ]
     then
         flirt \
             -in ${fsDir}/mri/brain.nii.gz \
             -ref ${dtiDir}/nodif_brain.nii.gz \
             -out ${regDir}/FREESURFERT1toNodif \
-            -omat ${regDir}/FREESURFERT1toNodif.mat \
+            -omat ${fs2nodifMat} \
             -bins 256 \
             -cost mutualinfo \
             -searchrx -180 180 \
@@ -46,13 +51,14 @@ do
     fi
 
     # FREESURFER output T1 --> diffusion kurtosis space
-    if [ ! -e ${regDir}/FREESURFERT1toDKINodif.mat ]
+    fs2dkinodifMat=${regDir}/FREESURFERT1toDKINodif.mat
+    if [ ! -e ${fs2dkinodifMat} ]
     then
         flirt \
             -in ${fsDir}/mri/brain.nii.gz \
             -ref ${dkiDir}/nodif_brain.nii.gz \
             -out ${regDir}/FREESURFERT1toDKINodif \
-            -omat ${regDir}/FREESURFERT1toDKINodif.mat \
+            -omat ${fs2dkinodifMat} \
             -bins 256 \
             -cost mutualinfo \
             -searchrx -180 180 \
@@ -62,19 +68,67 @@ do
     fi
 
     # MNI 2mm --> FREESURFER output T1 
-    if [ ! -e ${regDir}/MNItoFREESURFER.mat ]
+    mni2fs=${regDir}/MNItoFREESURFER.mat
+    if [ ! -e ${mni2fs} ]
     then
         flirt \
-            -in /usr/share/fsl/5.0/data/standard/MNI152_T1_2mm_brain.nii.gz \
+            -in ${mni2mm} \
             -ref ${fsDir}/mri/brain.nii.gz \
             -out ${regDir}/MNItoFREESURFER \
-            -omat ${regDir}/MNItoFREESURFER.mat \
+            -omat ${mni2fs} \
             -bins 256 \
             -cost mutualinfo \
             -searchrx -180 180 \
             -searchry -180 180 \
             -searchrz -180 180 \
             -dof 6  -interp trilinear
+    fi
+
+    #============================================================
+    # FNIRT MNI --> Freesurfer
+    #============================================================
+    mni2fs_fnirt=${regDir}/MNI_to_FREESURFER_fnirt_coeff.nii.gz
+    if [ ! -e ${mni2fs_fnirt} ]
+    then
+        fnirt \
+            --ref=${fsDir}/mri/brain.nii.gz \
+            --in=${mni2mm} \
+            --aff=${mni2fs} \
+            --inmask=${mni2mmMask} \
+            --refmask=${fsDir}/mri/brainmask.nii.gz \
+            --iout=${regDir}/MNI_to_FREESURFER_fnirt \
+            --cout=${mni2fs_fnirt}
+    fi
+    
+    # Freesurfer --> MNI
+    mni2fs2nodif=${regDir}/MNI_to_FREESURFER_fnirt_to_Nodif_flirt.nii.gz
+    if [ ! -e ${mni2fs2nodif} ]
+    then
+        convertwarp \
+            --ref=${dtiDir}/nodif_brain_mask.nii.gz \
+            --warp1=${mni2fs_fnirt} \
+            --postmat=${fs2nodifMat} \
+            --out=${mni2fs2nodif}
+    fi
+
+
+    nodif2fs2mni=${regDir}/Nodif_to_FREESURFER_flirt_to_MNI_fnirt.nii.gz
+    if [ ! -e ${nodif2fs2mni} ]
+    then
+        invwarp \
+            --ref=${mni2mm} \
+            --warp=${mni2fs2nodif} \
+            --out=${nodif2fs2mni}
+    fi
+
+    mni2nodif_fnfl_img=${regDir}/MNI_in_Nodif_FNIRT_FLIRT.nii.gz
+    if [ ! -e ${mni2nodif_fnfl_img} ]
+    then
+        applywarp \
+            --ref=${dtiDir}/nodif_brain.nii.gz \
+            --in=${mni2mm} \
+            --warp=${nodif2fs2mni} \
+            --out=${mni2nodif_fnfl_img}
     fi
 
     # Estimation of transformation matrix
@@ -88,7 +142,7 @@ do
     if [ ! -e ${regDir}/MNItoNodif.mat ]
     then
         convert_xfm -omat ${regDir}/MNItoNodif.mat \
-            -concat ${regDir}/FREESURFERT1toNodif.mat ${regDir}/MNItoFREESURFER.mat
+            -concat ${fs2nodifMat} ${mni2fs}
     fi
 
     # MNI --> FREESURFER --> DTI
