37a02934d8dad6b4047b9d4f3575ff3ac8831ffe
kcho <kang_ik.cho@kcl.ac.uk>
2018-09-07 00:30:04 +0900

    convert all images with thrP into MNI image

--- 05_T1_to_mni_registration.sh
+++ 05_T1_to_mni_registration.sh
@@ -6,9 +6,10 @@ do
     rawT1_mask_mgz=${i}/FREESURFER/mri/brainmask.mgz
     sourceImg=${rawT1}
     sourceImg_nobet=${i}/FREESURFER/mri/T1.nii.gz
-    targetImg=/usr/share/fsl/5.0/data/standard/MNI152_T1_1mm_brain.nii.gz
-    targetImg_nobet=/usr/share/fsl/5.0/data/standard/MNI152_T1_1mm.nii.gz
-    targetImgMask=/usr/share/fsl/5.0/data/standard/MNI152_T1_1mm_brain_mask.nii.gz
+
+    targetImg=/usr/local/fsl/data/standard/MNI152_T1_1mm_brain.nii.gz
+    targetImg_nobet=/usr/local/fsl/data/standard/MNI152_T1_1mm.nii.gz
+    targetImgMask=/usr/local/fsl/data/standard/MNI152_T1_1mm_brain_mask.nii.gz
 
     #make T1_brain.nii.gz
     if [ ! -e ${rawT1_mask} ]
@@ -55,15 +56,35 @@ do
 
         fi
 
-        for connectivity_thrp90 in ${i}/segmentation/${side}/90thrP/90_seeds_to*
+        for thrP in 5 10 15 20 25 30 35 40 45 50 55 60 65 70 75 80 85 90 95
         do
-            f_basename=`basename ${connectivity_thrp90}`
-            applywarp \
-                --ref=${targetImg} \
-                --in=${connectivity_thrp90} \
-                --warp=${i}/registration/T1_to_MNI_fnirt_coeff \
-                --out=${i}/segmentation/${side}/90thrP/mni_${f_basename} \
-                --interp=nn
+            for connectivity_thrp in ${i}/segmentation/${side}/${thrP}thrP/${thrP}_seeds_to*
+            do
+                f_basename=`basename ${connectivity_thrp}`
+                if [ ! -e ${i}/segmentation/${side}/${thrP}thrP/mni_${f_basename} ]
+                then
+                    applywarp \
+                        --ref=${targetImg} \
+                        --in=${connectivity_thrp} \
+                        --warp=${i}/registration/T1_to_MNI_fnirt_coeff \
+                        --out=${i}/segmentation/${side}/${thrP}thrP/mni_${f_basename} \
+                        --interp=nn
+                fi
+            done
+        done
+
+        for connectivity_thrp0 in ${i}/segmentation/${side}/seeds_to*
+        do
+            f_basename=`basename ${connectivity_thrp0}`
+            if [ ! -e ${i}/segmentation/${side}/mni_${f_basename} ]
+            then
+                applywarp \
+                    --ref=${targetImg} \
+                    --in=${connectivity_thrp0} \
+                    --warp=${i}/registration/T1_to_MNI_fnirt_coeff \
+                    --out=${i}/segmentation/${side}/mni_${f_basename} \
+                    --interp=nn
+            fi
         done
     done
 done
